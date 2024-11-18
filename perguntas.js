3d07d7478e2621ec7d9ea3cb3ea6380c885ac6e1
RAFAELAMARIA2024 <00001088782474SP@al.educacao.sp.gov.br>
2024-11-18 19:45:06 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Limpeza',
+    'Qual é o melhor detergente para lavar louça?',
+    'É o Ype transparente'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
 )
 
 criaCartao(
